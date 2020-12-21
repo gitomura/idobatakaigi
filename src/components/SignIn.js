59b61b208b9489b461f205a9a6f7313c0a5a677a
omura <omura@arbege.com>
2020-12-21 14:04:26 +0900

    save name in japanese

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -47,7 +47,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
-  console.log({ disabled, string });
+  const [isComposed, setIsComposed] = useState(false);
 
   useEffect(() => {
     const disabled = string === ''
@@ -72,12 +72,14 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e)=>{
-              console.log({key: e.key})
+              if(isComposed) return;
               if( e.key === 'Enter'){
                 setName(e.target.value)
                 e.preventDefault();
               }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
